ede5726ab660d494b0f422c49bd4a236d9f786fb
lookfiresu123 <lookfiresu123@gmail.com>
2016-01-26 23:59:44 +0800

    finish helloworld.java

---
+++ src/chapter_1/HelloWorld.java
@@ -0,0 +1,12 @@
+package chapter_1;
+
+/**
+ * Created by lookfiresu on 16-1-26.
+ */
+import java.lang.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
